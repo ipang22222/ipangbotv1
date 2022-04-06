a3efc823d12dde0bf948f47d573caab7326b345e
TipengOfc <100071150+ipang22222@users.noreply.github.com>
2022-04-06 05:16:18 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let path = require('path')
 let fs = require('fs')
 let package = require('./package.json')
 const CFonts  = require('cfonts')
-CFonts.say('NayLaBotz\nWhatsApp Bot', {
+CFonts.say('IpangBotz\nWhatsApp Bot', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
